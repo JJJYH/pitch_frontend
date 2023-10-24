fd04455b976d818755b622f8faf2e1428de3a086
JJJYH <jyh30011720@gmai.com>
2023-10-24 09:22:05 +0900

    231024 0921 jyh update common layout and update login function

--- src/store/toolkitStore.js
+++ src/store/toolkitStore.js
@@ -6,13 +6,15 @@ import educationReducer from './educationSlice';
 import careerReducer from './careerSlice';
 import profileReducer from './profileSlice';
 import skillReducer from './skillSlice';
+import userInfoReducer from './userInfoSlice';
 const toolkitStore = configureStore({
   reducer: {
     customization: customizationReducer,
     education: educationReducer,
     career: careerReducer,
     profile: profileReducer,
-    skill: skillReducer
+    skill: skillReducer,
+    userInfo: userInfoReducer
   }
 });
 
